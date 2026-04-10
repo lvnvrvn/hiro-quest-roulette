5e82d3f7628b9d1cb75ec8c5f3b353400f02c7fa
lvnvrvn <lvnvrvn@yandex.ru>
2026-04-10 13:57:14 +0300

    feat: add animated progress bar for 7 days

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -7,6 +7,7 @@ import Card from "../Card/Card";
 export default function App() {
   const [prizeIndex, setPrizeIndex] = useState<number | null>(null);
   const [isSpinning, setIsSpinning] = useState<boolean>(false);
+  const [currentDay, setCurrentDay] = useState<number>(1);
   const containerRef = useRef<HTMLDivElement>(null);
 
   const controls = useAnimation();
@@ -41,11 +42,20 @@ export default function App() {
 
     setIsSpinning(false);
     setPrizeIndex(randomIndex);
+
+    if (currentDay < 7) {
+      setCurrentDay((prev) => prev + 1);
+    }
   };
 
   return (
-    <div className="circle" ref={containerRef}>
-      <h1>Колесо фортуны</h1>
+    <div className="roulette" ref={containerRef}>
+      <h3 className="roulette__title">Колесо Фортуны </h3>
+      <p className="roulette__text">
+        Испытайте удачу раз в день
+        <br />и выигрывайте бонусы для VPN!
+      </p>
+      <div className="fortune__wheel__pic"></div>
 
       <motion.div className="cards__container" animate={controls}>
         {extendedPrizes.map((prize, index) => (
@@ -67,6 +77,26 @@ export default function App() {
         <span>{isSpinning ? "Крутим" : "Испытать удачу"}</span>
         <img src="../../public/btn-icon.png" alt="" />
       </button>
+
+      <p className="roulette__promotion__text">
+        Крути колесо 7 дней подряд без пропусков и получи на 7-й день
+        гарантированный 1 день подписки!
+      </p>
+      <div className="roulette__promotion__progress">
+        <motion.div
+          className="roulette__promotion__progress__fill"
+          initial={{ width: "14.28%" }}
+          animate={{ width: `${(currentDay / 7) * 100}%` }}
+          transition={{ duration: 0.8, ease: "circOut" }}
+        />
+        <div className="roulette__promotion__progress__dig">1</div>
+        <div className="roulette__promotion__progress__dig">2</div>
+        <div className="roulette__promotion__progress__dig">3</div>
+        <div className="roulette__promotion__progress__dig">4</div>
+        <div className="roulette__promotion__progress__dig">5</div>
+        <div className="roulette__promotion__progress__dig">6</div>
+        <div className="roulette__promotion__progress__dig week__prize">7</div>
+      </div>
     </div>
   );
 }
